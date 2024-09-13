ba20fd5cdcef61d04c5e4fdff4f3e6d9bf88d824
JhonCode <jjv20618@gmail.com>
2024-09-13 11:54:30 -0600

    Header changes
    Ahora el header puede mostrar la imagen del usuario en base a la url del recurso.

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,14 @@ return [
             'throw' => false,
         ],
 
+        'user_photos' =>[
+            'driver'=>'local',
+            'root' => storage_path('app/user_photos'),
+            'url' => env('APP_URL').'/user_photos',
+            'visibility' =>'public',
+            'throw'=>false
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -71,6 +79,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('user_photos') => storage_path('app/user_photos')
     ],
 
 ];
